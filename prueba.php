43274e371f47c3c6b75fec121a7bfe8565f6dd61
franrolando <frannrolando96@gmail.com>
2018-12-05 22:05:07 -0300

    Create prueba.php

---
+++ prueba.php
@@ -0,0 +1,12 @@
+<?php
+  $product = new Product();
+  $product->setName("Remera");
+  $product->setDescription("una remera nike");
+  $product->setPrice(400);
+  $product->setSex("M");
+  $product->setProductcode("AAA1");
+
+
+  echo $product;
+
+ ?>
